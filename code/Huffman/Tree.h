cc1ae56f2633eccd6c89a8a27fa5886666419fd6
DaniloDuque <dduque@estudiantec.cr>
2024-10-21 14:44:08 -0600

    ...

---
+++ code/Huffman/Tree.h
@@ -0,0 +1,14 @@
+#ifndef TREE_HEADER
+#define TREE_HEADER
+#define uchar unsigned char
+
+typedef struct {
+    int freq;
+    uchar data;
+    node *lft, node *rght;
+    node(int f, uchar d): freq(f), data(d), lft(nullptr), rght(nullptr){}
+    node(node *l, node *r): c('\0'), lft(l), rght(r), freq(l->freq + r->freq){}
+} node;
+
+
+#endif
