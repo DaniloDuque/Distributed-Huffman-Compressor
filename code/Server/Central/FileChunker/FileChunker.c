50eddd58ea03f2800080ebb17721fcf244d4afac
kencast <kencastillo2005@gmail.com>
2024-10-25 15:58:58 -0600

    Co-authored-by: AleCevar <AleCevar@users.noreply.github.com> Co-authored-by: Danilo <DaniloDuque@users.noreply.github.com> Co-authored-by: Luco1421 <Luco1421@users.noreply.github.com>

--- code/Server/Central/FileChunker/FileChunker.c
+++ code/Server/Central/FileChunker/FileChunker.c
@@ -1,6 +1,53 @@
-#include <stdio.h>
+#include "../util.h"
+
+#define RESOURCE_PATH "../resources/hola.txt"
+
+typedef struct{
+    ll basePosition;
+    ll chunkSize;
+} params;
+
+params* createParams(ll i, ll j){
+    params* p = (params*)malloc(sizeof(params)); 
+    p->basePosition = i;
+    p->chunkSize = j;
+    return p;
+}
+
+void* filePart(FILE* file, void* arg){
+    params *p = (params*)arg;
+    fseek(file, start, SEEK_SET);
+    uchar* filePart = (uchar*)malloc(p->chunkSize*(sizeof(uchar)));
+    for(ll i = 0;i<p->chunkSize; ++i)
+    {
+        uchar byte = fgetc(file);
+        if(byte == EOF) break;
+        filePart[i] = byte;
+    }
+}
+
+void splitFile(char* path, int numServers){
+    FILE* file;
+    file = fopen(path,"r");
+    if(file == NULL){
+        perror("Error al abrir el archivo");
+    }
+    fseek(file,0,SEEK_END);
+    ll size = ftell(file), chunkSize = (size / numServers);
+    fclose(file);
+    thread threads[numServers]; 
+    
+    for(ll i = 0; i < numServers ; i++) {
+        params* param = createParams(i * chunkSize, min(size, (i + 1) * chunkSize) - (i * chunkSize));
+        create(&threads[i], NULL, &filePart, param);
+    }
+    
+    for(int i = 0; i < numServers; i++){
+        join(&threads[i],NULL);
+    }
+}
 
 int main(){
-    
+
     return 0;
 }
--- code/Server/Central/util.h
+++ code/Server/Central/util.h
@@ -9,10 +9,33 @@
 #include <netinet/in.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <pthread.h>
 
 #define PORT 8585
 #define BUF_SIZE 1024 
 #define FILE_PATH "./resources/index.html"
+
+typedef long long ll;
+typedef pthread_t thread;
+typedef pthread_create create;
+typedef pthread_join join;
+typedef pthread_exit exit_thread;
+typedef pthread_detach detach;
+
+typedef pthread_mutex_t mutex;
+typedef pthread_mutex_init mutex_init;
+typedef pthread_mutex_lock mutex_lock;
+typedef pthread_mutex_trylock mutex_trylock;
+typedef pthread_mutex_unlock mutex_unlock;
+typedef pthread_mutex_destroy mutex_destroy;
+
+typedef pthread_cond_t cond;
+typedef pthread_cond_init cond_init;
+typedef pthread_cond_wait cond_wait;
+typedef pthread_cond_signal cond_signal;
+typedef pthread_cond_broadcast cond_broadcast;
+typedef pthread_cond_destroy cond_destroy;
+
 
 const char *error_response = 
     "HTTP/1.1 500 internal server error\r\n"
