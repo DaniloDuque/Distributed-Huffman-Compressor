066093a0825c336017b8c6c37585bfeaaf16913a
AleCevar <al.cerdas@estudiantec.cr>
2024-10-27 15:45:31 -0600

    savepoint f

--- code/Server/Central/FileCompressor/FileCompressor.h
+++ code/Server/Central/FileCompressor/FileCompressor.h
@@ -3,68 +3,25 @@
 
 #include "../util.h"
 extern int dto[MAX_SIZE*2];
-
-mutex cmp = PTHREAD_MUTEX_INITIALIZER;
+mutex dto_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void* sendRoutes(void* arg) {
     int* exitCode = (int*)malloc(sizeof(int));
-    client_info* info = (client_info*) arg;
     if (!exitCode) {
         perror("Error in malloc");
-        close(info->socket);
-        *exitCode=-1;
-        exit_t(exitCode);
         return NULL;
     }
-    if(send(info->socket, (int*)dto, 2*MAX_SIZE*sizeof(int), 0) < 0) {
+    int socket = *((int*) arg);
+    if(send(socket, (int*)dto, 2*MAX_SIZE*sizeof(int), 0) < 0) {
         perror("Error sending the code table");
         *exitCode = -1;
-        close(info->socket);
-        exit_t(exitCode);
-        return NULL;
+        close(socket);
+        return exitCode;
     }
-    ll partSize;
-    // lock(&cmp);
-    if(recv(info->socket, &partSize, sizeof(partSize), 0)==-1){
-        perror("Error receiving the compressed part");
-        *exitCode=-1;
-        close(info->socket);
-        exit_t(exitCode);
-        return NULL;
-    }
-    printf("Bytes received of compressed part %lld\n", partSize);
-    char ruta[0x19];
-    sprintf(ruta, "%s%d", SAVED_FILE_ROUTE, info->index); 
-    fprintf(stderr,"%s\n", ruta);
-    FILE* filePart=fopen(ruta, "wb");
-    if(filePart==NULL){
-        perror("Error creating the compressed part file\n");
-        *exitCode=-1;
-        close(info->socket);
-        exit_t(exitCode);
-        return NULL;
-    }
-    ll total_received=0;
-    ssize_t bytes_received;
-    uchar * buffer[BUFFER_SIZE];
-    while (total_received < partSize && 
-           (bytes_received = recv(info->socket, buffer, BUFFER_SIZE, 0)) > 0) {
-        fwrite(buffer, 1, bytes_received, filePart);
-        total_received += bytes_received;
-    }
-    if(total_received!=partSize){
-        fprintf(stderr, "bytes do not match, received: %lld expected: %lld\n", total_received, partSize);
-        *exitCode=-1;
-        close(info->socket);
-        // unlock(&cmp);
-        exit_t(exitCode);
-        return NULL;
-    }
-    // unlock(&cmp);
-    fclose(filePart);
-    close(info->socket);
+    
+    close(socket);
     *exitCode = 0;
-    exit_t(exitCode);
+    return exitCode;
 }
 
 bool compressFile(int n, client_info* clients) {
@@ -74,23 +31,26 @@ bool compressFile(int n, client_info* clients) {
     bool flag = true;
     
     for(int i = 0; i < n; i++) {
-        if(create(&threads[i], NULL, &sendRoutes, &clients[i]) != 0) {
+        if(create(&threads[i], NULL, &sendRoutes, &clients[i].socket) != 0) {
             perror("Error creating threads for compression");
+            for(int j = 0; j < i; j++) {
+                void* exitCode;
+                join(threads[j], &exitCode);
+                free(exitCode);
+            }
             return false;
         }
     }
-
-    int* exitCode;
+    
     for(int i = 0; i < n; i++) {
-        if(join(threads[i],(void **)&exitCode) != 0 || *exitCode != 0){
-            fprintf(stderr,"Thread %d crashed while compressing\n",i);    
+        void* exitCode;
+        if(join(threads[i], &exitCode) != 0 || (exitCode && *(int*)exitCode != 0)) {
             flag = false;
         }
+        if (exitCode) free(exitCode);
     }
-    free(exitCode);
-    if(flag == false) return false;
-    return true;
-
+    
+    return flag;
 }
 
 #endif
--- code/Server/Workers/FileCompressor/FileCompressor.h
+++ code/Server/Workers/FileCompressor/FileCompressor.h
@@ -4,6 +4,158 @@
 #include "../util.h"
 
 bool compress(int* codes, int socket) {
+    // if (!codes) return false;
+    
+    // int byte = 0;
+    // int bit = 7;
+    // FILE* fileR = fopen(OUTPUT_FILE, "rb");    
+    // if(fileR == NULL) {
+    //     perror("Error al abrir el archivo de lectura");
+    //     return false;
+    // }
+    
+    // FILE* fileW = fopen(PATH_COMPRESS, "wb");
+    // if(fileW == NULL) {
+    //     perror("Error al abrir el archivo de escritura");
+    //     fclose(fileR);
+    //     return false;
+    // }
+        
+    // size_t bytesRead;
+
+    // uchar buffer[BUFFER_SIZE];
+    // uchar cbuffer[BUFFER_SIZE];
+
+    // size_t compressPos;
+
+    // unsigned char byte = 0;
+    // int bit = 7;
+
+    // size_t totalBytesWritten = 0;
+
+    // while((bytesRead = fread(buffer, sizeof(char), BUFFER_SIZE, fileR)) > 0) {
+    //     compressPos = 0;
+        
+    //     if (ferror(fileR)) {
+    //         perror("Error al leer el archivo de entrada");
+    //         fclose(fileW);
+    //         fclose(fileR);
+    //         return false;
+    //     }
+
+    //     for(size_t i = 0; i < bytesRead; i++) {
+    //         unsigned int ascii = (unsigned int)buffer[i];
+            
+    //         if (codes[2*ascii] <= 0 || codes[2*ascii] > MAX_SIZE) {
+    //             fprintf(stderr, "Código inválido encontrado para el carácter %d\n", ascii);
+    //             fclose(fileW);
+    //             fclose(fileR);
+    //             return false;
+    //         }
+
+    //         for(int j = 0; j < codes[2*ascii]; j++) {
+    //             if (compressPos >= BUFFER_SIZE - 1) {
+    //                 if (fwrite(cbuffer, 1, compressPos, fileW) != compressPos) {
+    //                     perror("Error al escribir en el archivo comprimido");
+    //                     fclose(fileW);
+    //                     fclose(fileR);
+    //                     return false;
+    //                 }
+    //                 totalBytesWritten += compressPos;
+    //                 compressPos = 0;
+    //             }
+
+    //             if(TEST(codes[2*ascii+1], j)) SET(byte, bit);
+    //             bit--;
+                
+    //             if(bit == -1) {
+    //                 cbuffer[compressPos++] = byte;
+    //                 byte = 0;
+    //                 bit = 7;
+    //             }
+    //         }
+    //     }
+        
+    //     if (compressPos > 0) {
+    //         if (fwrite(cbuffer, 1, compressPos, fileW) != compressPos) {
+    //             perror("Error al escribir en el archivo comprimido");
+    //             fclose(fileW);
+    //             fclose(fileR);
+    //             return false;
+    //         }
+    //         totalBytesWritten += compressPos;
+    //     }
+    // }
+
+    // if (bit != 7) {
+    //     unsigned char finalBytes[2];
+    //     finalBytes[0] = byte;
+    //     finalBytes[1] = 7 - bit;
+        
+    //     if (fwrite(finalBytes, 1, 2, fileW) != 2) {
+    //         perror("Error al escribir los bits finales en el archivo comprimido");
+    //         fclose(fileW);
+    //         fclose(fileR);
+    //         return false;
+    //     }
+    //     totalBytesWritten += 2;
+    // }
+
+    // printf("Compresión completada. Total de bytes escritos: %zu\n", totalBytesWritten);
+    
+    // if(remove(OUTPUT_FILE) != 0) {
+    //     perror("Error al eliminar el archivo");
+    //     return false;
+    // }
+ 
+    // fileW = fopen(PATH_COMPRESS,"rb");
+    // if(fileW == NULL){
+    //     perror("Error al abrir el archivo antes de enviar");
+    //     return false;
+    // }
+    
+    // fseek(fileW,0,SEEK_END);
+    // ll file_size = ftell(fileW);
+    // fseek(fileW, 0, SEEK_SET);
+    // if(send(socket, &file_size , sizeof(file_size), 0) < 0){
+    //     perror("Error al enviar el size");
+    //     return false;
+    // }
+    // printf("Bytes send of compressed part %lld\n", file_size);
+    
+    // ll remainingBytes = file_size;
+    // ll totalSent = 0;
+    // while(remainingBytes > 0){
+    //     size_t bytesToRead = (remainingBytes < BUFFER_SIZE) ? remainingBytes : BUFFER_SIZE;
+    //     size_t bytesRead = fread(buffer, 1, bytesToRead, fileW);
+        
+    //     if(bytesRead <= 0) {
+    //         if(feof(fileW)) break;  
+    //         perror("Error sending file");
+    //         return false;
+    //     }
+    //     ssize_t bytesSent = send(socket, buffer, bytesRead, 0);
+    //     if(bytesSent == -1) {
+    //         perror("Error while sending file");
+    //         return false;
+    //     }
+    //     totalSent += bytesSent;
+    //     remainingBytes -= bytesSent;
+    // }
+    // if(totalSent != file_size) {
+    //     fprintf(stderr, "Error: Sent %lld bytes of %lld expected\n", totalSent,file_size);
+    //     return false;
+    // }   
+    
+    // // if(remove(PATH_COMPRESS) != 0) {
+    // //     perror("Error al eliminar el archivo");
+    // //     return false;
+    // // }
+    
+//    return true;
+//}
+
+
     if (!codes) return false;
     
     int byte = 0;
@@ -30,12 +182,14 @@ bool compress(int* codes, int socket) {
         compressPos = 0;
         for(int i = 0; i < bytesRead && compressPos < BUFFER_SIZE; i++) {
             unsigned int ascii = (unsigned int)buffer[i];
+            if (ascii >= MAX_SIZE) continue;
             for(int j = 0; j < codes[2*ascii] && compressPos < BUFFER_SIZE; j++) {
                 if(TEST(codes[2*ascii+1], j)) SET(byte, bit);
                 bit--;
                 if(bit == -1) {
-                    cbuffer[compressPos++] = byte;
+                    cbuffer[compressPos] = byte;
                     byte = 0;
+                    compressPos++;
                     bit = 7;
                 }
             }
@@ -52,55 +206,12 @@ bool compress(int* codes, int socket) {
     fclose(fileW);
     fclose(fileR);
     
-    if(remove(OUTPUT_FILE) != 0) {
-        perror("Error al eliminar el archivo");
-        return false;
-    }
- 
-    fileW = fopen(PATH_COMPRESS,"rb");
-    if(fileW == NULL){
-        perror("Error al abrir el archivo antes de enviar");
-        return false;
-    }
-    fseek(fileW,0,SEEK_END);
-    ll file_size = ftell(fileW);
-    fseek(fileW, 0, SEEK_SET);
-    if(send(socket, &file_size , sizeof(file_size), 0) < 0){
-        perror("Error al enviar el size");
-        return false;
-    }
-    printf("Bytes send of compressed part %lld\n", file_size);
-    
-    ll remainingBytes = file_size;
-    ll totalSent = 0;
-    while(remainingBytes > 0){
-        size_t bytesToRead = (remainingBytes < BUFFER_SIZE) ? remainingBytes : BUFFER_SIZE;
-        size_t bytesRead = fread(buffer, 1, bytesToRead, fileW);
-        
-        if(bytesRead <= 0) {
-            if(feof(fileW)) break;  
-            perror("Error sending file");
-            return false;
-        }
-        ssize_t bytesSent = send(socket, buffer, bytesRead, 0);
-        if(bytesSent == -1) {
-            perror("Error while sending file");
-            return false;
-        }
-        totalSent += bytesSent;
-        remainingBytes -= bytesSent;
-    }
-    if(totalSent != file_size) {
-        fprintf(stderr, "Error: Sent %lld bytes of %lld expected\n", totalSent,file_size);
-        return false;
-    }   
-    
-    // if(remove(PATH_COMPRESS) != 0) {
+    // if(remove(OUTPUT_FILE) != 0) {
     //     perror("Error al eliminar el archivo");
     //     return false;
     // }
+    
     return true;
 }
-
 #endif
 
